deb01cbf4caec8ba9f449bd7860e7d6c5b9d47d4
Yannik Sprengart <yannik.sprengart@dfki.de>
2022-04-09 01:03:05 +0200

    Add book textures

--- js/book.js
+++ js/book.js
@@ -13,8 +13,31 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 canvas.appendChild(renderer.domElement);
 
+// Lighting
+const ambientLight = new THREE.AmbientLight(0x404040); // soft white light
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(0, 0, 6);
+scene.add(directionalLight);
+
+// Texture
+const urls = [
+  "https://uploads-ssl.webflow.com/624ee00c6c94893ddfff4251/6250b9632ec86461ca6b55c3_right.png",
+  "https://uploads-ssl.webflow.com/624ee00c6c94893ddfff4251/6250b9643b567d278cc915ea_left.png",
+  "https://uploads-ssl.webflow.com/624ee00c6c94893ddfff4251/6250bc43363baf31093fd97a_top.png",
+  "https://uploads-ssl.webflow.com/624ee00c6c94893ddfff4251/6250b96398308e0dbcd814a8_bottom.png",
+  "https://uploads-ssl.webflow.com/624ee00c6c94893ddfff4251/6250b9642ac18df17c72bbbf_front.png",
+  "https://uploads-ssl.webflow.com/624ee00c6c94893ddfff4251/6250bc4466c242f02ed704de_Back.png",
+];
+
+const loader = new THREE.TextureLoader();
+
+const material = urls.map((url) => {
+  return new THREE.MeshBasicMaterial({ map: loader.load(url) });
+});
+
 const geometry = new THREE.BoxGeometry(4, 6.17, 0.6);
-const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
@@ -22,7 +45,7 @@ camera.position.z = 8;
 
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
+  // cube.rotation.x += 0.005;
   cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 }

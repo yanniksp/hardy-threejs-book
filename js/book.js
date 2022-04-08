77f95872659d3c007853df8fe6911599a7a91de9
Yannik Sprengart <yannik.sprengart@dfki.de>
2022-04-09 00:28:57 +0200

    Change camera perspective

--- js/book.js
+++ js/book.js
@@ -18,7 +18,7 @@ const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = 8;
 
 function animate() {
   requestAnimationFrame(animate);
